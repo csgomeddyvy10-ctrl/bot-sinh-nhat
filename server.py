2a44968074b4a095e5b1eb3963c6d13364262353
csgomeddyvy10-ctrl <csgomeddyvy10@gmail.com>
2026-07-24 10:55:13 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import os
 import threading
 from dotenv import load_dotenv
 from flask import Flask
-import bot  # Import bot.py
 
 load_dotenv()
 
@@ -16,20 +15,15 @@ def ping():
 def home():
     return "Birthday Bot is running!", 200
 
-# Chạy Flask server
 def run_flask():
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
-# Chạy Discord Bot
 def run_bot():
     import bot
     bot.run_bot()
 
 if __name__ == "__main__":
-    # Chạy Flask trong thread
     flask_thread = threading.Thread(target=run_flask, daemon=True)
     flask_thread.start()
-    
-    # Chạy bot
     run_bot()
